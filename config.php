b34a55824aeae0db3b324623c515c30ec4dc222d
koshitaku <95271922+koshitaku@users.noreply.github.com>
2021-12-01 16:40:08 +0900

    Add files via upload

---
+++ config.php
@@ -0,0 +1,21 @@
+
+<?php
+error_reporting(0);
+ob_start();
+session_start();
+date_default_timezone_set('Asia/Tokyo');
+$DBHOST = "us-cdbr-east-04.cleardb.com";
+$DBPORT = "3306";
+$DBNAME = "heroku_1bdb8dde0683137";
+$DBUSER = "b9cf8537cbb86a";
+$DBPASS = "13f5bfba@us-cdbr-east-04";
+//2021/3/17 pg_connect‚ÉC³ 
+$connStr = "host=$DBHOST port=$DBPORT dbname=$DBNAME user=$DBUSER password=$DBPASS";
+$conn = pg_connect($connStr) or die("can't connect db");
+//var_dump($conn);
+
+/* try {$conn = new PDO("pgsql:host=$DBHOST;port=$DBPORT;dbname=$DBNAME;user=$DBUSER;password=$DBPASS");}
+catch (PDOException $e) {
+	echo 'DBÚ‘±ƒGƒ‰[F ' . $e->getMessage();
+} */
+?>
