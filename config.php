2ebbbd5a15eaa033661a9ac0251b02ee2e2a485b
koshitaku <95271922+koshitaku@users.noreply.github.com>
2021-12-01 17:50:51 +0900

    Update config.php

--- config.php
+++ config.php
@@ -13,7 +13,7 @@ date_default_timezone_set('Asia/Tokyo');
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
+$cleardb_password = $cleardb_url["password"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
