94cc7e5c671fb84c6a386a522d7c8790160e4bf8
koshitaku <95271922+koshitaku@users.noreply.github.com>
2021-12-05 01:48:26 +0900

    Update config.php

--- config.php
+++ config.php
@@ -15,28 +15,15 @@ try {
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = 'SELECT * FROM user';
-    $prepare = $db->prepare($sql);
-    $prepare->execute();
 
-    echo '<pre>';
     $prepare->execute();
     $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
     print_r(h($result));
-    echo "\n";
-    echo '</pre>';
+
 } catch (PDOException $e) {
     echo 'Error: ' . h($e->getMessage());
 }
 
-function h($var)
-{
-    if (is_array($var)) {
-        return array_map('h', $var);
-    } else {
-        return htmlspecialchars($var, ENT_QUOTES, 'UTF-8');
-    }
-}
 
 
 
