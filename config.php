9c97946a73370666a16ab8730fc848d13c80ea3d
koshitaku <95271922+koshitaku@users.noreply.github.com>
2021-12-05 01:51:37 +0900

    Update config.php

--- config.php
+++ config.php
@@ -5,26 +5,20 @@ session_start();
 date_default_timezone_set('Asia/Tokyo');
 
 
-
-$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-$db['dbname'] = ltrim($db['path'], '/');
-$dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-
-try {
-    $db = new PDO($dsn, $db['user'], $db['pass']);
-    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-    $prepare->execute();
-    $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
-    print_r(h($result));
-
-} catch (PDOException $e) {
-    echo 'Error: ' . h($e->getMessage());
+function dbConnect(){
+  $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+  $db['dbname'] = ltrim($db['path'], '/');
+  $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+  $user = $db['user'];
+  $password = $db['pass'];
+  $options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+  );
+  $dbh = new PDO($dsn,$user,$password,$options);
+  return $dbh;
 }
-
-
 
 
 
