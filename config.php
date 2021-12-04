883426b98ee8d3de4a59985db5572593fcd8d70f
koshitaku <95271922+koshitaku@users.noreply.github.com>
2021-12-05 01:41:14 +0900

    Update config.php

--- config.php
+++ config.php
@@ -4,16 +4,39 @@ ob_start();
 session_start();
 date_default_timezone_set('Asia/Tokyo');
 
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
+$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+$db['dbname'] = ltrim($db['path'], '/');
+$dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+
+try {
+    $db = new PDO($dsn, $db['user'], $db['pass']);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = 'SELECT * FROM user';
+    $prepare = $db->prepare($sql);
+    $prepare->execute();
+
+    echo '<pre>';
+    $prepare->execute();
+    $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+    print_r(h($result));
+    echo "\n";
+    echo '</pre>';
+} catch (PDOException $e) {
+    echo 'Error: ' . h($e->getMessage());
+}
+
+function h($var)
+{
+    if (is_array($var)) {
+        return array_map('h', $var);
+    } else {
+        return htmlspecialchars($var, ENT_QUOTES, 'UTF-8');
+    }
+}
 
 
 
