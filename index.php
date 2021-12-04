5d31f61fbb664e4f6d6fd80da4dac79c4e51be88
koshitaku <95271922+koshitaku@users.noreply.github.com>
2021-12-05 02:03:12 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,6 @@
 <?php
-error_reporting(0);
-ob_start();
 session_start();
-date_default_timezone_set('Asia/Tokyo');
-
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
-
-
-
+include("config.php");
 ?><!doctype html>
 <html>
   <head>
