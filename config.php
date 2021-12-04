06336dca3b65728c2b64b221b437f07851b12a38
koshitaku <95271922+koshitaku@users.noreply.github.com>
2021-12-04 12:23:02 +0900

    Update config.php

--- config.php
+++ config.php
@@ -5,15 +5,15 @@ session_start();
 date_default_timezone_set('Asia/Tokyo');
 
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("https://www.cleardb.com/database/details?id=7B4B85F84C198768722848FCFFA85F44"));
-$cleardb_host = "us-cdbr-east-04.cleardb.com";
-$cleardb_username = "b65770169c839b";
-$cleardb_password = "16639ad0";
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_host, $cleardb_username, $cleardb_password, $cleardb_db);
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 
 
