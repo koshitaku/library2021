9cb4d662913c0861ae47ffd415cd2a0e45e34585
koshitaku <95271922+koshitaku@users.noreply.github.com>
2021-12-05 02:00:28 +0900

    Update tulinmedia.php

--- tulinmedia.php
+++ tulinmedia.php
@@ -1,6 +1,27 @@
 <?php
+error_reporting(0);
+ob_start();
 session_start();
-include("config.php");
+date_default_timezone_set('Asia/Tokyo');
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
+
+
+?>
+<?php
+
+
 include("top.php");?>
 <style>
   .carousel-inner img {
